af52e6e905887987b9c93845838b1411418652d2
mike-cook-projects <mike.z.cook@gmail.com>
2013-02-28 22:26:53 -0800

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,15 +1,15 @@
-//////////////////////////////////////////////////////////////////////////////////////////
-// MATRIX PUZZLE SOLVER  							     								//
-// Author: Mike Cook								     								//
-// Date: 09/03/12									     								//
-// Description: Solves a matrix puzzle with the following conditions:					//
-// 				1. Each tile gives the point value on the tile							//
-// 				2. Tiles marked "D" double the current point total						//
-// 				3. Once crossed, a tile provides no additional points					//
-// 																						//
-// 				With those requirements, this library solves a matrix of random			//
-// 				dimensions and starting point by showing the optimal route.				//
-//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// MATRIX PUZZLE SOLVER  							     		//
+// Author: Mike Cook								     		//
+// Date: 09/03/12									     	//
+// Description: Solves a matrix puzzle with the following conditions:				//
+// 				1. Each tile gives the point value on the tile			//
+// 				2. Tiles marked "D" double the current point total		//
+// 				3. Once crossed, a tile provides no additional points		//
+// 												//
+// 				With those requirements, this library solves a matrix of random	//
+// 				dimensions and starting point by showing the optimal route.	//
+//////////////////////////////////////////////////////////////////////////////////////////////////
 var MatrixSolver = {
 	// "Constants"
 	ROW_LIMIT: 5,
